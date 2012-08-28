e5de394a5d8cce3d308cac964f1be6dc874390fd
Patrik Akselsson <patrik.akselsson@gmail.com>
2012-08-28 21:54:58 +0200

    initial commit, empty rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FriendListJssdk::Application.config.secret_token = 'e0c0d4c37193efc52beb5f09d6135d3981c96fa1a02e295746b284c1378b02b0cf9a9eeef435eed0ad669cd3e7965cd97a9f8f0966ed414075c78008cef36835'

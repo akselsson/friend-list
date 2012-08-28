3b76e5cd354c88a272f87c7e081584c232482e2b
Patrik Akselsson <patrik.akselsson@gmail.com>
2012-08-28 22:13:36 +0200

    added facebook jssdk

--- config/application.rb
+++ config/application.rb
@@ -64,5 +64,7 @@ module FriendListJssdk
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.facebook_app_id = ENV['FACEBOOK_APP_ID']
   end
 end

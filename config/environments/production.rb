6a268f3a9f966e43e7ac8c6f0e1521f839562b5b
Patrik Akselsson <patrik.akselsson@gmail.com>
2012-08-31 14:10:26 +0200

    updated production app id

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,8 @@ FriendListJssdk::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+  #
+  #
+  config.facebook_app_id = '403597616361135'
+
 end
